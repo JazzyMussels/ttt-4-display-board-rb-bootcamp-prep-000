3c2d0627829cae81cfc8834563b52fb4872e3717
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-13 14:56:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,10 @@
 # out the current state.
 
 def display_board(board)
- rows[0] =  "   |   |   "
- rows[1] =  "-----------"
- rows[2] =  "   |   |   "
- rows[3] =  "-----------"
- rows[4] =  "   |   |   "
+ puts " #{board[0]}  |  #{board[1]} | #{board[2]}  "
+ puts "-----------"
+ puts " #{board[3]}  |  #{board[4]} | #{board[5]}  "
+ puts "-----------"
+ puts " #{board[6]}  |  #{board[7]} | #{board[8]}  "
 end
 
