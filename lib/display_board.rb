865ac2aef006a8a507dc9670a8b8ca34180b3989
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-13 14:49:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+ rows[0] =  "   |   |   "
+ rows[1] =  "-----------"
+ rows[2] =  "   |   |   "
+ rows[3] =  "-----------"
+ rows[4] =  "   |   |   "
+end
+
